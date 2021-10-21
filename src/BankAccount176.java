e68b05900a1a5fcc33217c820c6971e9cc8027dc
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2021-10-21 19:27:48 -0400

    reupload of assignment

--- src/BankAccount176.java
+++ src/BankAccount176.java
@@ -1,9 +1,89 @@
 
 public class BankAccount176 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	long accountNum;
+	String name;
+	boolean overdrawn;
+	private double balance;
+	
+	//constructor
+	public BankAccount176(String name, long accountNum, double balance, boolean overdrawn) {
+		this.name = name;
+		this.accountNum = accountNum;
+		this.balance = balance;
+		this.overdrawn = overdrawn;
 	}
-
-}
+	
+	
+	//gets the account number
+	public long getAccountNum(long number) {
+		accountNum = number;
+		return number;
+	}
+	
+	//sets the account number
+	public void setAccountNum(long acctnum) {
+		accountNum = acctnum;
+	}
+	
+	//gets the account owner's name
+	public String getAccountName(String name){
+		this.name = name;
+		return name;
+	}
+	
+	//sets the account owner's name
+	public void setAccountName(String acctname) {
+		name = acctname;
+	}
+	
+	//gets account overdrawn status
+	public boolean isOverdrawn(boolean overdrawn) {
+		return overdrawn;
+	}
+	
+	//sets overdrawn status
+	public void setOverdrawn(boolean status) {
+		overdrawn = status;
+	}
+	
+	//withdraw method takes money from the account
+	public void withdraw(double amount) {
+		if (overdrawn) {
+           System.out.println("Insufficient funds");
+        } else balance = balance - amount;
+			System.out.println("You have withdrawn: " + amount);
+		if(balance < 0) {
+			overdrawn = true;
+		}
+	}
+	
+	//deposit method will add to balance
+	public void deposit(double amount) {
+		balance = balance + amount;
+		System.out.println("You have deposited: " + amount);
+		if(balance > 0) {
+			overdrawn = false;
+		}
+	}
+	
+	//transfer method moves amount from one account to another
+	public void transfer(double amount, BankAccount176 otherAccount) {
+		this.withdraw(amount);
+		otherAccount.deposit(amount);
+	}
+	
+	//addInterest method is used to add interest to balance
+	public void addInterest() {
+		balance = balance + (balance * .035);
+	}
+	
+	//toString method will print out a nice clean summary of the account in a string
+	public String toString() {
+		String toString;
+		if(overdrawn) {
+			 toString = "The account number " + accountNum + " is owned by " + name + " with a Balance " + balance + ". It is overdrawn";
+		}else { toString= "The account number " + accountNum + " is owned by " + name + " with a Balance " + balance + ". It is not overdrawn";
+		 }return toString;
+	}
+	
+}	
--- src/BankAccountTest176.java
+++ src/BankAccountTest176.java
@@ -1,8 +1,21 @@
 
 public class BankAccountTest176 {
-
+//*TO PRINT OUT OVERDRAWN STATUS AND BALANCE I USED THE toString METHOD BECAUSE THERE WERE NO OTHER METHODS TO CALL TO PRINT JUST THOSE THINGS
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BankAccount176 harrysChecking = new BankAccount176("Harry", 12345, 0.00, false);
+		System.out.println(harrysChecking.toString());
+		harrysChecking.deposit(200.00);
+		System.out.println(harrysChecking.toString());
+		harrysChecking.withdraw(500.00);
+		System.out.println(harrysChecking.toString());
+		harrysChecking.deposit(400.00);
+		System.out.println(harrysChecking.toString());
+		BankAccount176 harrysSavings = new BankAccount176("Harry", 86754, 500.00, false);
+		System.out.println(harrysSavings.toString());
+		harrysChecking.transfer(50.00, harrysSavings);
+		System.out.println(harrysChecking.toString());
+		System.out.println(harrysSavings.toString());
+		
 
 	}
 
